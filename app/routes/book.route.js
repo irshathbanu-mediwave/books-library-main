189a654e3bb2a2cc63a015e7fb493adf0924bd40
irshathbanu-mediwave <irshathbanu@mindwaveventures.com>
2023-11-06 11:16:21 +0530

    add validate

--- app/routes/book.route.js
+++ app/routes/book.route.js
@@ -1,3 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
+const { validate } = require("../middlewares/validate.middleware");
+const { bookSchema } = require("../validations/book.schema");
+const {
+  getAllbooksController,
+  deletebooksController,
+  addbooksController,
+  addByid,
+  updatebooksController,
+} = require("../controllers/book.controllres");
+
+router.get("/", getAllbooksController);
+router.get("/:id", addByid);
+router.post("/", validate(bookSchema), addbooksController);
+router.put("/:id", validate(bookSchema), updatebooksController);
+router.delete("/:id", deletebooksController);
+
+module.exports = router;
