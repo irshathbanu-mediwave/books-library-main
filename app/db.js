1cf06ec9fbcd9c1b8a229a7058ff46a1ef7137c4
irshathbanu-mediwave <irshathbanu@mindwaveventures.com>
2023-11-07 17:22:53 +0530

    add validation

--- app/db.js
+++ app/db.js
@@ -3,58 +3,146 @@ const { isValidISBN } = require("./isbn");
 
 const books = [
   {
-    id: "ybybynjn",
+    id:"132",
     title: "testing",
-    isbn: 1861972717,
+    isbn: "1861972717",
   },
 ];
+const bookdsratings = [
+  {
+    id: 102,
+    rating: 5,
+    bookId:1505,
+  },
+];
+//all books
 const allbooks = () => books;
-const getbook = ({ title, isbn }) => {
+// newbooks
+const addonebook = ({ title, isbn }) => {
+  const bookid = uuidv4();
   if (isValidISBN(isbn)) {
-    const id = uuidv4();
     const b = {
-      id,
+      id: bookid,
       title,
       isbn,
     };
     books.push(b);
+    return b;
   } else console.log("Invalid");
+};
+const addRating = ({ rating, bookId }) => {
+  const ratingId = uuidv4();
+
+  const bookRating = {
+    id: ratingId,
+    rating,
+    bookId,
+  };
+  bookdsratings.push(bookRating);
+  return bookRating;
+};
+const getbookbyid = (id) => {
+  const book = books.find((b) => b.id == id);
+  if (!book) {
+    return null;
+  }
+  const ratingentry = bookdsratings.find((b) => b.bookId == id);
+  const rating = ratingentry ? ratingentry.rating : 0;
+  b = {
+    id: book.id,
+    title: book.title,
+    isbn: book.isbn,
+    rating: rating,
+  };
   return b;
 };
-const addbyid = ({ id }) => {
-  const idx = books.findIndex((b) => b.id === id);
-  if (idx === -1) {
+const updatebooks = ({ id, title }) => {
+  const idx = books.findIndex((b) => b.id == id);
+  if (idx != -1) {
+    books[idx]["title"] = title;
+
+    return books[idx];
+  }
+  return null;
+};
+const deletebook = ( id ) => {
+  const idx = books.findIndex((b) => b.id == id);
+  const ratingidx= bookdsratings.findIndex((b) => b.bookId == id);
+  if (idx == -1) {
     return null;
   }
-  return books[idx];
+
+ const b=books[idx];
+ books.splice(idx,1)
+ if (ratingidx !== -1) {
+  const r = bookdsratings[ratingidx];
+  bookdsratings.splice(ratingidx, 1);
+  return {
+    b,
+    r,
+  };
+}
+return b;
 };
 
-const updatebooks = ({ id, payload }) => {
-  const idx = books.findIndex((b) => b.id == id);
-  if (idx === -1) {
-    return null;
+const updaterating = ({ rating, bookId }) => {
+  const idx = bookdsratings.findIndex((b) => b.bookId == bookId);
+  if (idx != -1) {
+    bookdsratings [idx]["rating"] = rating;
+    return bookdsratings[idx];
   }
-
-  books[idx]["title"] = payload["title"];
-  books[idx]["isbn"] = payload["isbn"];
-  return books[idx];
+  return null;
 };
 
-const deletebook = ({ id }) => {
-  const idx = books.findIndex((b) => b.id == id);
-  if (idx === -1) {
+const getratingbyid = (id) => {
+  const rating = bookdsratings.find((r) => r.id == id);
+  if (!rating) {
     return null;
   }
-
-  const book = books[idx];
-  book.splice(idx, 1);
-  return book;
+  const book = books.find((b) => b.id == rating.bookId);
+  return {
+    id: rating.id,
+    rating: rating.rating,
+    book,
+  };
+};
+const deleteratingbyid = (id) => {
+  const idx =bookdsratings.findIndex((r) => r.id == id);
+  if (idx == -1) {
+    return null;
+  }
+  const deletedRating = bookdsratings[idx];
+  bookdsratings.splice(idx, 1);
+  return deletedRating;
 };
 
 module.exports = {
   updatebooks,
-  addbyid,
+  getbookbyid,
   deletebook,
   allbooks,
-  getbook,
+  addonebook ,
+  addRating,
+  updaterating ,
+  getratingbyid ,
+  deleteratingbyid 
+
 };
+
+//single books
+// const addbyid = ({ id }) => {
+//   const idx = books.findIndex((b) => b.id === id);
+//   if (idx === -1) {
+//     return null;
+//   }
+//   return books[idx];
+// };
+
+//
+
+//
+
+//   const book = books[idx];
+//   book.splice(idx, 1);
+//   return book;
+// };
--- app/index.js
+++ app/index.js
@@ -2,18 +2,147 @@ const express = require("express");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 dotenv.config();
-const { notfound } = require("./middlewares/notfound.middleware");
-const { errorHandler } = require("./middlewares/errorhandler.middleware");
-const Bookrouter=require("./routes/book.route")
+
+const {
+  updatebooks,
+  getbookbyid,
+  deletebook,
+  allbooks,
+  addonebook,
+  addRating,
+  updaterating,
+  getratingbyid,
+  deleteratingbyid,
+} = require("./db");
+const Joi = require("joi");
 const app = express();
 const config = require("./config");
 app.use(express.json());
 app.use(morgan("dev"));
+//READ for books
+app.get("/books", (req, res) => {
+  const books = allbooks();
+  res.send(books);
+});
+app.get("/books", (req, res) => {});
+app.get("/books/:bookid", (req, res) => {
+  const book = getbookbyid(req.query.bookid);
+  console.log(book + req.query.bookid);
+  if (!book) {
+    return res.status(400).json({
+      message: "book not found",
+    });
+  }
+  return res.send(book);
+});
+//POST for book
+app.post("/books", (req, res) => {
+  const booksSchema = Joi.object({
+    title: Joi.string().required(),
+    isbn: Joi.string().required(),
+  });
+  const { value, error } = booksSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json({
+      message: error.details.map((d) => d.message),
+    });
+  }
+  const book = addonebook(value);
+  return res.send(book);
+});
 
-app.use("/books",Bookrouter);
+//put for book
+app.put("/books/:bookid", (req, res) => {
+  const editschema = Joi.object({
+    title: Joi.string().required(),
+  });
+  const { value, error } = editschema.validate(req.body);
+  if (error) {
+    return res.status(400).json({
+      message: error.details.map((m) => m.message),
+    });
+  }
+  const book = updatebooks({ id: req.params.bookid, title: req.body.title });
 
-app.use(notfound);
-app.use(errorHandler);
+  if (!book) {
+    return res.status(400).json({
+      message: "book not found",
+    });
+  }
+  return res.send(book);
+});
+// delete books
+app.delete("/books/:bookid", (req, res) => {
+  const book = deletebook(req.params.bookid);
+  if (!book) {
+    return res.status(400).json({
+      message: "book not found",
+    });
+  }
+  return res.send(book);
+});
+//get for rating
+app.get("/rating/:ratingid", (req, res) => {
+  const book = getratingbyid(req.params.ratingid);
+  if (!book) {
+    return res.status(400).json({
+      message: "rating not found",
+    });
+  }
+  return res.send(book);
+});
+// delete for rating
+app.delete("/rating/:ratingid", (req, res) => {
+  const rating = deleteratingbyid(req.params.ratingid);
+  if (!rating) {
+    return res.status(400).json({
+      message: "rating not found ",
+    });
+  }
+  return res.json(rating);
+});
+// put for rating
+app.put("/books/:bookid/rating", (req, res) => {
+  const rating = updaterating({
+    rating: req.body.rating,
+    bookId: req.params.bookid,
+  });
+  if (!rating) {
+    return res.status(400).json({
+      message: "rating not found",
+    });
+  }
+  const ratingSchema = Joi.object({
+    rating: Joi.number().min(0).max(5).required(),
+  });
+
+  const { value, error } = ratingSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json({
+      message: error.details.map((d) => d.message),
+    });
+  }
+
+  return res.json(rating);
+});
+//POST for rating
+app.post("/books/:bookid/rating", (req, res) => {
+  const ratingSchema = Joi.object({
+    rating: Joi.number().min(0).max(5).required(),
+  });
+
+  const { value, error } = ratingSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json({
+      message: error.details.map((d) => d.message),
+    });
+  }
+  const rating = addRating({
+    rating: req.body.rating,
+    bookId: req.params.bookid,
+  });
+  return res.json(rating);
+});
 app.listen(config.appPort, () => {
   console.log(`Server running on ${config.appPort}`);
 });
